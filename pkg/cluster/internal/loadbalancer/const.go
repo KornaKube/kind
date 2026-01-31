0a01e867881f469dfcc8af382f5c83a0c25c8779
Benjamin Elder <bentheelder@google.com>
2026-01-30 21:57:30 -0800

    bump haproxy image

--- pkg/cluster/internal/loadbalancer/const.go
+++ pkg/cluster/internal/loadbalancer/const.go
@@ -17,7 +17,7 @@ limitations under the License.
 package loadbalancer
 
 // Image defines the loadbalancer image:tag
-const Image = "docker.io/kindest/haproxy:v20251211-v0.29.0-alpha-100-g82a92c5d"
+const Image = "docker.io/kindest/haproxy:v20260131-7181c60a"
 
 // ConfigPath defines the path to the config file in the image
 const ConfigPath = "/usr/local/etc/haproxy/haproxy.cfg"
